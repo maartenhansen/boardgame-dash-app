d9355ff7c3103eeedcb373478a08239e211d6292
Maarten Hansen <maarten.hansen98@gmail.com>
2023-04-21 13:36:54 +0200

    Added function to create initial df with games

--- functions/sql_bg_dash.py
+++ functions/sql_bg_dash.py
@@ -1,5 +1,31 @@
 from sqlalchemy import create_engine, text, Table, MetaData, Column, Integer, insert, select
 from sqlalchemy.exc import IntegrityError
+import pandas as pd
 
+
+# create engine for connection with database
 def create_db_connection(server, database):
     return create_engine("mssql+pyodbc:///?odbc_connect=DRIVER={ODBC Driver 17 for SQL Server};SERVER=" + server + ";DATABASE=" + database + ";Trusted_Connection=yes;")
+
+
+def create_initial_df(engine):
+    # create initial dataframe from fact table
+    with engine.connect() as conn:
+        # only select maxplaytime, because minplaytime & maxplaytime will always be similar, so we would count the value double
+        stmt = text("select ID, BgNumber, BgName, MinPlayers, MaxPlayers, MaxPlaytime, Complexity, Ranking from Fct_Boardgame Where RowEndDate IS NULL AND Ranking < 1500 AND YearPublished != 0 ORDER BY Ranking Asc")
+        bgg_df = pd.read_sql(stmt, conn)
+
+    # Create list with all the Dim columns that need to be added
+    dim_list = ['Category', 'Designer', 'Illustrator', 'Mechanic', 'Subdomain', 'Publisher']
+        
+    # Run a query to add dimension values to dataframe
+    with engine.connect() as conn:
+        for dim in dim_list: # Run the query for each Dimension in dim_list
+            # sql statement that returns all values for each game concatenated (divided using ,-,)
+            stmt = text(f"select Fct.BgNumber, STRING_AGG(CONVERT(VARCHAR(max), Dim.{dim}Name), ', ') AS {dim}List, Fct.Ranking from Fct_Boardgame as Fct left join Dim_Boardgame_{dim} as Joi on Joi.Boardgame_ID=Fct.ID left join Dim_{dim} as Dim on Joi.{dim}_ID=Dim.ID Where RowEndDate IS NULL AND Ranking < 1500 AND YearPublished != 0 Group By Fct.BgNumber, Fct.Ranking Order by Fct.Ranking Asc")
+            # create temporary df
+            df_temp = pd.read_sql(stmt, conn)
+            # add dimension column form temporary df to final df
+            bgg_df[f"{dim}List"] = df_temp[f"{dim}List"]
+
+    return bgg_df
--- pages/aanbevelingen.py
+++ pages/aanbevelingen.py
@@ -18,7 +18,7 @@ import time
 # import custom handwritten functions
 import sys
 sys.path.append('C:/Users/maart/OneDrive/Bureaublad/Syntra/Eindwerk/Dash-Plotly') # make sure python will look into the 'functions' folder as well
-from functions.sql_bg_dash import create_db_connection
+from functions.sql_bg_dash import create_db_connection, create_initial_df
 
 dash.register_page(__name__, path='/recommender/')
 
@@ -37,23 +37,7 @@ database = 'BoardgameProject'
 engine = create_db_connection(server, database)
 
 # create initial dataframe from fact table
-with engine.connect() as conn:
-    # only select maxplaytime, because minplaytime & maxplaytime will always be similar, so we would count the value double
-    stmt = text("select ID, BgNumber, BgName, MinPlayers, MaxPlayers, MaxPlaytime, Complexity, Ranking from Fct_Boardgame Where RowEndDate IS NULL AND Ranking < 1500 AND YearPublished != 0 ORDER BY Ranking Asc")
-    bgg_df = pd.read_sql(stmt, conn)
-
-# Create list with all the Dim columns that need to be added
-dim_list = ['Category', 'Designer', 'Illustrator', 'Mechanic', 'Subdomain', 'Publisher']
-    
-# Run a query to add dimension values to dataframe
-with engine.connect() as conn:
-    for dim in dim_list: # Run the query for each Dimension in dim_list
-        # sql statement that returns all values for each game concatenated (divided using ,-,)
-        stmt = text(f"select Fct.BgNumber, STRING_AGG(CONVERT(VARCHAR(max), Dim.{dim}Name), ', ') AS {dim}List, Fct.Ranking from Fct_Boardgame as Fct left join Dim_Boardgame_{dim} as Joi on Joi.Boardgame_ID=Fct.ID left join Dim_{dim} as Dim on Joi.{dim}_ID=Dim.ID Where RowEndDate IS NULL AND Ranking < 1500 AND YearPublished != 0 Group By Fct.BgNumber, Fct.Ranking Order by Fct.Ranking Asc")
-        # create temporary df
-        df_temp = pd.read_sql(stmt, conn)
-        # add dimension column form temporary df to final df
-        bgg_df[f"{dim}List"] = df_temp[f"{dim}List"]
+bgg_df = create_initial_df(engine)
     
 bgg_df_cols_to_display = [
     {"name": "BGG-ranking", "id": "Ranking"},

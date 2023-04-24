4155a0cd94e8e3dde0762650171c610cfad9d87b
Maarten Hansen <maarten.hansen98@gmail.com>
2023-04-24 15:12:10 +0200

    CHanged top to 250

--- pages/statistieken.py
+++ pages/statistieken.py
@@ -13,6 +13,8 @@ load_figure_template("superhero")
 import sys
 sys.path.append('C:/Users/maart/OneDrive/Bureaublad/Syntra/Eindwerk/Dash-Plotly') # make sure python will look into the entire 'Dash-Plotly' folder as well
 from functions.sql_bg_dash import create_db_connection, get_game_amount_by_designer_top_x, get_game_name_by_designer_top_x
+from functions.filters import comp_filter_ranking, exe_filter_ranking # import functions for defining/creating filter components
+
 
 # Define page & path
 dash.register_page(__name__, path='/stats/') # '/' = homepage
@@ -25,12 +27,15 @@ database = 'BoardgameProject'
 # create engine for connection with database
 engine = create_db_connection(server, database)
 
-### Create barchart with amount of times designers are in top 100
-df_top_x_designers = get_game_amount_by_designer_top_x(engine, 100)
+### Create filter for ranking
+filter_ranking = comp_filter_ranking()
+
+### Create barchart with amount of times designers are in top 250
+df_top_x_designers = get_game_amount_by_designer_top_x(engine, 250)
 fig_top_x_designers =  px.bar(df_top_x_designers, x='Auteur', y='Aantal')
 
 ### Create datatable to show the exact games of the designers in the above barchart
-df_top_x_designers_gamenames = get_game_name_by_designer_top_x(engine, 100)
+df_top_x_designers_gamenames = get_game_name_by_designer_top_x(engine, 250)
 df_top_x_designers_gamenames_cols_to_display = [{"name": "Bordspel", "id": "Bordspel"}]
 top_x_designers_gamenames_table = dash_table.DataTable(
     data=df_top_x_designers_gamenames.to_dict('records'),
@@ -64,15 +69,15 @@ layout = html.Div([
     html.Div([
         html.H1(children='Statistieken'),
         html.Div([
-            html.H2(children='Bordspellen in de top 100'),
+            html.H2(children='Bordspellen in de top 250'),
             dcc.Graph(
-                id='designers_in_top_100', 
+                id='designers_in_top_250', 
                 figure=fig_top_x_designers,
                 config={'displayModeBar': False}
                 ),
             ], style={'width': '47.5%', 'display': 'inline-block', 'marginRight': '5%'}),
         html.Div([
-            html.H2(children='Bordspellen in de top 100'),
+            html.H2(children='Bordspellen in de top 250'),
             top_x_designers_gamenames_table
             ], style={'width': '47.5%', 'display': 'inline-block', 'vertical-align': 'top'}
             ),
@@ -82,7 +87,7 @@ layout = html.Div([
 ###### Add callbacks (allows components to interact)
 @callback(
     Output(component_id='top_x_designers_gamenames', component_property='data'),
-    Input(component_id='designers_in_top_100', component_property='hoverData')
+    Input(component_id='designers_in_top_250', component_property='hoverData')
 )
 def update_top_x_designers_gamenames(hoverdata):
     try:
